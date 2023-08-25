2e57c232fba18ce41ef4f5eb409915c639c8f5f7
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-08-25 17:49:53 -0500

    create three additional tests that will evaluate whether the hit() and isSunk() functions are properly affecting the object

--- src/shipFactory.test.js
+++ src/shipFactory.test.js
@@ -7,3 +7,31 @@ test("returns an object with the proper length, hits, and sunkStatus properties"
     expect(carrier.hits).toBe(0);
     expect(carrier.sunkStatus).toBe(false);
 });
+
+test("increases the number of hits on the ship when hit() is invoked", () => {
+    const carrier = shipFactory(5);
+    carrier.hit();
+    carrier.hit();
+
+    expect(carrier.hits).toBe(2);
+});
+
+test("evaluates whether the ship has been sunk depending on the number of hits it has sustained (false test)", () => {
+    const carrier = shipFactory(5);
+    carrier.hit();
+    carrier.isSunk();
+
+    expect(carrier.sunkStatus).toBe(false);
+
+});
+
+test("evaluates whether the ship has been sunk depending on the number of hits it has sustained (true test)", () => {
+    const carrier = shipFactory(5);
+    for (let i = 0; i < 5; i++) {
+        carrier.hit();
+    }
+    carrier.isSunk();
+
+    expect(carrier.sunkStatus).toBe(true);
+    
+});
