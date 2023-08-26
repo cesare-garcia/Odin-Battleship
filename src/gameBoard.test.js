71d6aa6c9914bd794ab681ba55623395eced7b63
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-08-26 17:05:59 -0500

    create new tests for the receiveAttack function and set other tests to skip

--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -1,25 +1,24 @@
 const gameBoardFactory = require("./gameBoard");
-// may need to const ship for some of these
 
-// test("the gameboard Factory function returns an object with a board property housing an array with 10 elements, a property that counts sunken ships, and a property that determines whether the game is over", () => {
-//     const gameBoardOne = gameBoardFactory();
+test.skip("the gameboard Factory function returns an object with a board property housing an array with 10 elements, a property that counts sunken ships, and a property that determines whether the game is over", () => {
+    const gameBoardOne = gameBoardFactory();
 
-//     expect(gameBoardOne.board.length).toBe(10);
-//     expect(gameBoardOne.board[0].length).toBe(10);
-//     expect(gameBoardOne.board[1].length).toBe(10);
-//     expect(gameBoardOne.board[2].length).toBe(10);
-//     expect(gameBoardOne.board[3].length).toBe(10);
-//     expect(gameBoardOne.board[4].length).toBe(10);
-//     expect(gameBoardOne.board[5].length).toBe(10);
-//     expect(gameBoardOne.board[6].length).toBe(10);
-//     expect(gameBoardOne.board[7].length).toBe(10);
-//     expect(gameBoardOne.board[8].length).toBe(10);
-//     expect(gameBoardOne.board[9].length).toBe(10);
-//     expect(gameBoardOne.sunkenCount).toBe(0);
-//     expect(gameBoardOne.gameOver).toBe(false);
-// });
+    expect(gameBoardOne.board.length).toBe(10);
+    expect(gameBoardOne.board[0].length).toBe(10);
+    expect(gameBoardOne.board[1].length).toBe(10);
+    expect(gameBoardOne.board[2].length).toBe(10);
+    expect(gameBoardOne.board[3].length).toBe(10);
+    expect(gameBoardOne.board[4].length).toBe(10);
+    expect(gameBoardOne.board[5].length).toBe(10);
+    expect(gameBoardOne.board[6].length).toBe(10);
+    expect(gameBoardOne.board[7].length).toBe(10);
+    expect(gameBoardOne.board[8].length).toBe(10);
+    expect(gameBoardOne.board[9].length).toBe(10);
+    expect(gameBoardOne.sunkenCount).toBe(0);
+    expect(gameBoardOne.gameOver).toBe(false);
+});
 
-test("Calls the ship Factory function and places it properly at a starting coordinate with a horizontal orientation (destroyer - success).", () => {
+test.skip("Calls the ship Factory function and places it properly at a starting coordinate with a horizontal orientation (destroyer - success).", () => {
     
     const gameBoardOne = gameBoardFactory();
     gameBoardOne.placeShip(2, 3, 3);
@@ -32,7 +31,7 @@ test("Calls the ship Factory function and places it properly at a starting coord
     expect(gameBoardOne.board[3][4].shipLength).toBe(2);
 });
 
-test("Calls the ship Factory function and places it properly at a starting coordinate with a horizontal orientation (carrier - success).", () => {
+test.skip("Calls the ship Factory function and places it properly at a starting coordinate with a horizontal orientation (carrier - success).", () => {
     
     const gameBoardOne = gameBoardFactory();
     gameBoardOne.placeShip(5, 3, 3);
@@ -55,19 +54,19 @@ test("Calls the ship Factory function and places it properly at a starting coord
 
 });
 
-test("Calls the ship Factory function and does NOT place it at the starting Coord because there isn't space for it (destroyer - horizontal failure).", () => {
+test.skip("Calls the ship Factory function and does NOT place it at the starting Coord because there isn't space for it (destroyer - horizontal failure).", () => {
     
     const gameBoardOne = gameBoardFactory();
     expect(gameBoardOne.placeShip(2, 9, 9)).toBe("Not enough space. Cannot place destroyer here.");
 });
 
-test("Calls the ship Factory function and does NOT place it at the starting Coord because there isn't space for it (carrier - horizontal failure).", () => {
+test.skip("Calls the ship Factory function and does NOT place it at the starting Coord because there isn't space for it (carrier - horizontal failure).", () => {
     
     const gameBoardOne = gameBoardFactory();
     expect(gameBoardOne.placeShip(5, 6, 6)).toBe("Not enough space. Cannot place carrier here.");
 });
 
-test("Calls the ship Factory function and places it properly at a starting coordinate with a vertical orientation (carrier - success).", () => {
+test.skip("Calls the ship Factory function and places it properly at a starting coordinate with a vertical orientation (carrier - success).", () => {
     
     const gameBoardOne = gameBoardFactory();
     gameBoardOne.rotateShip();
@@ -92,7 +91,7 @@ test("Calls the ship Factory function and places it properly at a starting coord
 
 });
 
-test("Calls the ship Factory function and places it properly at a starting coordinate with a vertical orientation (destroyer - success).", () => {
+test.skip("Calls the ship Factory function and places it properly at a starting coordinate with a vertical orientation (destroyer - success).", () => {
     
     const gameBoardOne = gameBoardFactory();
     gameBoardOne.rotateShip();
@@ -106,44 +105,53 @@ test("Calls the ship Factory function and places it properly at a starting coord
     expect(gameBoardOne.board[4][3].shipLength).toBe(2);
 });
 
-test("Calls the ship Factory function and does NOT place it at the starting Coord because there isn't space for it (destroyer - vertical failure).", () => {
+test.skip("Calls the ship Factory function and does NOT place it at the starting Coord because there isn't space for it (destroyer - vertical failure).", () => {
     
     const gameBoardOne = gameBoardFactory();
     gameBoardOne.rotateShip();
     expect(gameBoardOne.placeShip(2, 9, 9)).toBe("Not enough space. Cannot place destroyer here.");
 });
 
-test("Calls the ship Factory function and does NOT place it at the starting Coord because there isn't space for it (carrier - vertical failure).", () => {
+test.skip("Calls the ship Factory function and does NOT place it at the starting Coord because there isn't space for it (carrier - vertical failure).", () => {
     
     const gameBoardOne = gameBoardFactory();
     gameBoardOne.rotateShip();
     expect(gameBoardOne.placeShip(5, 6, 6)).toBe("Not enough space. Cannot place carrier here.");
 });
 
-
-// test("Calls the ship Factory function and places it properly at a starting coordinate with a vertical orientation", () => {
+test("Takes a pair of coordinates and correctly determines the ship has been hit, sends the hit to the correct ship, and changes element of coordinate to X (horizontal test).", () => {
+    const gameBoardOne = gameBoardFactory();
+    gameBoardOne.placeShip(2,2,2);
+    gameBoardOne.receiveAttack(2,2);
+    gameBoardOne.receiveAttack(2,3);
     
-//     const gameBoardOne = gameBoardFactory();
-//     const patrol = shipFactory(2);
-//     gameBoardOne.placeShip(patrol, 3, 3);
+    expect(gameBoardOne.board[2][2]).toBe("X");
+    expect(gameBoardOne.board[2][3]).toBe("X");
+    expect(gameBoardOne.getSunkenCount()).toBe(1);
+    expect(gameBoardOne.receiveAttack(2,2)).toBe("You cannot strike the same spot twice");
+});
+
+test("Takes a pair of coordinates and correctly determines the ship has been hit, sends the hit to the correct ship, and changes element of coordinate to X (vertical test).", () => {
+    const gameBoardOne = gameBoardFactory();
+    gameBoardOne.rotateShip();
+    gameBoardOne.placeShip(2,2,2);
+    gameBoardOne.receiveAttack(2,2);
+    gameBoardOne.receiveAttack(2,3);
+    gameBoardOne.receiveAttack(3,2);
     
-//     expect(gameBoardOne.board[3,3]).toBe(patrol);
-//     expect(gameBoardOne.board[4,3]).toBe(patrol);
-// });
+    expect(gameBoardOne.board[2][2]).toBe("X");
+    expect(gameBoardOne.board[2][3]).toBe("M");
+    expect(gameBoardOne.board[3][2]).toBe("X");
+    expect(gameBoardOne.getSunkenCount()).toBe(1);
+    expect(gameBoardOne.receiveAttack(2,2)).toBe("You cannot strike the same spot twice");
+});
 
-// test("Takes a pair of coordinates and correctly determines the ship has been hit, sends the hit to the correct ship, and changes element of coordinate to X.", () => {
-//     const gameBoardOne = gameBoardFactory();
-//     gameBoardOne.receivesAttack(2,2);
-    
-//     expect(gameBoardOne.board[2,2]).toBe("X");
-// });
+test.skip("Records an attack as missed (M) within the gameboard if it does not hit a ship.", () => {
+    const gameBoardOne = gameBoardFactory();
+    gameBoardOne.receiveAttack(0,0);
 
-// test("Records an attack as missed (M) within the gameboard if it does not hit a ship.", () => {
-//     const gameBoardOne = gameBoardFactory();
-//     gameBoardOne.receivesAttack(0,0);
-
-//     expect(gameBoardOne.board[0][0]).toBe("M");
-// });
+    expect(gameBoardOne.board[0][0]).toBe("M");
+});
 
 // test("Correctly reports that all ships have been sunk.", () => {
     
