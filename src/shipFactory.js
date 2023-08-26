970f4ac47f502d356bac6bbe9e6ac7e05566401f
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-08-26 12:10:56 -0500

    add orientation property and rotate method to the object returned by the factory function

--- src/shipFactory.js
+++ src/shipFactory.js
@@ -1,14 +1,20 @@
+// we will potentially need to include an orientation property that describes whether the ship is horizontal or vertical
+
 const shipFactory = (length) => {
    
     return {
         length: length,
         hits: 0,
         sunkStatus: false,
+        orientation: "horizontal",
         hit: function() {
             this.hits++;
         },
         isSunk: function() {
             if (this.length === this.hits) this.sunkStatus = true;
+        },
+        rotate: function() {
+            (this.orientation === "horizontal") ? this.orientation = "vertical" : this.orientation = "horizontal";
         }
     };
 };
