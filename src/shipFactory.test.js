5d5d6d672df50d01883fcaa9e17666c046a83761
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-08-26 12:10:23 -0500

    add new tests for the orientation property and rotate function of the ship factory function

--- src/shipFactory.test.js
+++ src/shipFactory.test.js
@@ -1,11 +1,12 @@
 const shipFactory = require("./shipFactory");
 
-test("returns an object with the proper length, hits, and sunkStatus properties", () => {
+test("returns an object with the proper length, hits, sunkStatus, and orientation properties", () => {
     const carrier = shipFactory(5);
 
     expect(carrier.length).toBe(5);
     expect(carrier.hits).toBe(0);
     expect(carrier.sunkStatus).toBe(false);
+    expect(carrier.orientation).toBe("horizontal");
 });
 
 test("increases the number of hits on the ship when hit() is invoked", () => {
@@ -22,7 +23,6 @@ test("evaluates whether the ship has been sunk depending on the number of hits i
     carrier.isSunk();
 
     expect(carrier.sunkStatus).toBe(false);
-
 });
 
 test("evaluates whether the ship has been sunk depending on the number of hits it has sustained (true test)", () => {
@@ -33,5 +33,19 @@ test("evaluates whether the ship has been sunk depending on the number of hits i
     carrier.isSunk();
 
     expect(carrier.sunkStatus).toBe(true);
-    
+});
+
+test("changes the ship's orientation property to vertical from horizontal", () => {
+    const patrol = shipFactory(2);
+    patrol.rotate();
+
+    expect(patrol.orientation).toBe("vertical");
+});
+
+test("changes the ship's orientation property to horizontal from vertical", () => {
+    const patrol = shipFactory(2);
+    patrol.rotate();
+    patrol.rotate();
+
+    expect(patrol.orientation).toBe("horizontal");
 });
