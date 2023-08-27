6ad9a0ee5c9aaaa50e690be7add5ad1c7ead2e26
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-08-27 17:03:38 -0500

    create initial functions for player and AI creation

---
+++ src/createPlayer.js
@@ -0,0 +1,31 @@
+const createPlayer = (name, order) => {
+
+    const launchAttack = (yCoord, xCoord) => {
+        return [yCoord, xCoord];
+    };
+
+    return {
+        name,
+        order,
+        launchAttack
+    };
+};
+// module.exports = createPlayer;
+
+const createAI = () => {
+    
+    launchAttack = () => {
+        let yCoord = Math.floor(Math.random()*11);
+        let xCoord = Math.floor(Math.random()*11);
+
+        // probably will use some type of loop to do this
+        // code to check if the xys are duplicates or not
+    };
+    
+    return {
+        name: "Enemy AI",
+        order: 2,
+        launchAttack
+    };
+};
+module.exports = createAI;
